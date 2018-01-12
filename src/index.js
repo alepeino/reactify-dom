fd4c5df02fb8fa629e35e113b387024012535d92
Alejandro Peinó <alepeino@gmail.com>
2018-01-12 17:53:49 -0300

    Render w/children

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import { kebabCase } from 'lodash/fp'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const NODE_TYPE = {
+  ELEMENT: 1,
+  TEXT: 3,
+  COMMENT: 8
+}
+
 const components = {}
 
 function reactTree (node) {
-  const element = components[Object.keys(components).find(selector => node.matches(selector))]
-  return React.createElement(element)
+  if (node.nodeType === NODE_TYPE.COMMENT) {
+    return
+  }
+
+  if (node.nodeType === NODE_TYPE.TEXT) {
+    return node.textContent
+  }
+
+  const element =
+    components[Object.keys(components).find(selector => node.matches(selector))]
+    || node.localName
+  const children = Array.from(node.childNodes).map(reactTree)
+  return React.createElement(element, {}, ...children)
 }
 
 const ReactifyDOM = {
--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,8 @@ import ReactifyDOM, { registerComponent } from '../src'
 import { expectDomToContain } from './helpers'
 import {
   TestComponent1,
-  TestComponent2
+  TestComponent2,
+  UlComponent
 } from './stubs'
 
 beforeEach(ReactifyDOM.clearComponents)
@@ -26,16 +27,7 @@ describe('component registration', () => {
 
 })
 
-describe('rendering to DOM', () => {
-
-  test('yields identical DOM when no components mounted', () => {
-    const html = `<html><head></head><body><h1>Title</h1><div>Body</div></body></html>`
-    const dom = new JSDOM(html)
-
-    ReactifyDOM.render(dom.window.document.body)
-
-    expect(dom.serialize()).toEqual(html)
-  })
+describe('rendering components', () => {
 
   test('unregistered component renders as unknown element', () => {
     const dom = new JSDOM(`<html><head></head><body><h1>Title</h1><test-component-1>Unknown HTML Element</test-component-1></body></html>`)
@@ -99,6 +91,97 @@ describe('rendering to DOM', () => {
       expect(node.innerHTML).toEqual('<div><p>Test Component 2</p></div>'),
       2
     )
+  })
+
+  describe('render components with children', () => {
+    test('children are registered components', () => {
+      const dom = new JSDOM(`<html>
+        <head></head>
+        <body>
+            <ul-component><test-component-1></test-component-1><test-component-1></test-component-1></ul-component>
+        </body>
+      </html>`)
+
+      ReactifyDOM.registerComponent(UlComponent)
+        .registerComponent(TestComponent1)
+        .render(dom.window.document.body)
+
+      expectDomToContain(dom, 'ul-component > *', node => {
+        expect(node.tagName).toEqual('UL')
+      }, 1)
+      expectDomToContain(dom, 'ul-component > * > *', node => {
+        expect(node.tagName).toEqual('DIV')
+        expect(node.innerHTML).toEqual('Test Component 1')
+      }, 2)
+    })
+
+    test('children are standard HTML tags', () => {
+      const dom = new JSDOM(`<html>
+        <head></head>
+        <body>
+            <ul-component>
+              <li>HTML tag as child</li>
+              <li>HTML tag as child</li>
+            </ul-component>
+        </body>
+      </html>`)
+
+      ReactifyDOM.registerComponent(UlComponent)
+        .registerComponent(TestComponent1)
+        .render(dom.window.document.body)
+
+      expectDomToContain(dom, 'ul-component > *', node => {
+        expect(node.tagName).toEqual('UL')
+      }, 1)
+      expectDomToContain(dom, 'ul-component > * > *', node => {
+        expect(node.tagName).toEqual('LI')
+        expect(node.textContent).toEqual('HTML tag as child')
+      }, 2)
+    })
+
+    test('children include text nodes', () => {
+      const dom = new JSDOM(`<html>
+        <head></head>
+        <body>
+            <ul-component>
+              Text Node 1
+              <test-component-1></test-component-1>
+              Text Node 2
+            </ul-component>
+        </body>
+      </html>`)
+
+      ReactifyDOM.registerComponent(UlComponent)
+        .registerComponent(TestComponent1)
+        .render(dom.window.document.body)
+
+      expectDomToContain(dom, 'ul-component', node => {
+        expect(node.innerHTML).toContain('Text Node 1')
+        expect(node.innerHTML).toContain('Text Node 2')
+      })
+    })
+
+    test('comment nodes are removed from children', () => {
+      const dom = new JSDOM(`<html>
+        <head></head>
+        <body>
+            <ul-component>
+              <!--Comment node 1-->
+              <test-component-1></test-component-1>
+              <!--Comment node 2-->
+            </ul-component>
+        </body>
+      </html>`)
+
+      ReactifyDOM.registerComponent(UlComponent)
+        .registerComponent(TestComponent1)
+        .render(dom.window.document.body)
+
+      expectDomToContain(dom, 'ul-component', node => {
+        expect(node.innerHTML).not.toContain('Comment Node 1')
+        expect(node.innerHTML).not.toContain('Comment Node 2')
+      })
+    })
   })
 })
 
